2d3e346a3e6a3b0e382cb2ea03ea8a7d243109bc
darshan-1798 <112754624+darshan-1798@users.noreply.github.com>
2022-12-12 22:44:21 +0530

    code is fixed

--- prime.js
+++ prime.js
@@ -2,12 +2,15 @@ let x=20;
 for(let i=2;i<=x;i++)
 {
 let y=2;
-while(y*y<i)
+let isPrime=true;
+while(y**y<=i)
 {
-if(i%y==0)
+if(i%y==0){
 isPrime=false;
-
+break;
 }
-if(isPrime=true)
+y++
+}
+if(isPrime==true)
 console.log(i);
 }
